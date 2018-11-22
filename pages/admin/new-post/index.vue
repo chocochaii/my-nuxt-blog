a45f152918a54cceeb8f259f89d65c322b795a9f
Unknown <thanat.apiwattanadej@gmail.com>
2018-11-23 02:10:28 +0700

    [added] axios and firebase

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,30 +1,39 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-      <AdminPostForm />
+      <AdminPostForm @submit="onSubmit" />
     </section>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import AdminPostForm from "@/components/admin/AdminPostForm";
 
 export default {
   layout: "admin",
   components: {
     AdminPostForm
+  },
+  methods: {
+    onSubmit(postData) {
+      axios
+        .post("https://my-nuxt-blog-f8547.firebaseio.com/posts.json", postData)
+        .then(result => console.log(result))
+        .catch(e => console.log(e));
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.new-post-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
   .new-post-form {
-    width: 90%;
-    margin: 20px auto;
+    width: 500px;
   }
-  @media (min-width: 768px) {
-    .new-post-form {
-      width: 500px;
-    }
-  }
+}
 </style>
--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from "vuex";
+import axios from "axios";
 
 const createStore = () => {
   return new Vuex.Store({
@@ -12,41 +13,16 @@ const createStore = () => {
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            vuexContext.commit("setPosts", [
-              {
-                id: "1",
-                thumbnail:
-                  "https://i2-prod.mirror.co.uk/incoming/article11812659.ece/ALTERNATES/s1200/The-Feline-World-Gathers-For-The-Supreme-Cat-Show-2017.jpg",
-                title: "Meow World!",
-                previewText: "Meow's gonna rule this world"
-              },
-              {
-                id: "2",
-                thumbnail:
-                  "https://i2-prod.mirror.co.uk/incoming/article11812659.ece/ALTERNATES/s1200/The-Feline-World-Gathers-For-The-Supreme-Cat-Show-2017.jpg",
-                title: "Meow Sun!",
-                previewText: "Meow's gonna rule the Sun"
-              },
-              {
-                id: "3",
-                thumbnail:
-                  "https://i2-prod.mirror.co.uk/incoming/article11812659.ece/ALTERNATES/s1200/The-Feline-World-Gathers-For-The-Supreme-Cat-Show-2017.jpg",
-                title: "Meow Moon!",
-                previewText: "Meow's gonna rule the Moon"
-              },
-              {
-                id: "4",
-                thumbnail:
-                  "https://i2-prod.mirror.co.uk/incoming/article11812659.ece/ALTERNATES/s1200/The-Feline-World-Gathers-For-The-Supreme-Cat-Show-2017.jpg",
-                title: "Meow Mars!",
-                previewText: "Meow's gonna rule the Mars"
-              }
-            ]);
-            resolve();
-          }, 1000);
-        });
+        return axios
+          .get("https://my-nuxt-blog-f8547.firebaseio.com/posts.json")
+          .then(res => {
+            const postsArray = [];
+            for (const key in res.data) {
+              postsArray.push({ ...res.data[key], id: key });
+            }
+            vuexContext.commit("setPosts", postsArray);
+          })
+          .catch(e => context.error(e));
       },
       setPosts(vuexContext, posts) {
         vuexContext.commit("setPosts", posts);
